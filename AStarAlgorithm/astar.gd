5af1b898b097a99c2b9fadd2edfbc02c9a32449a
GemDust <a.bant@bant-web.nl>
2025-02-22 19:42:34 -0500

    V1.0
    Implemented basic A* and UI

---
+++ AStarAlgorithm/astar.gd
@@ -0,0 +1,139 @@
+class_name AStar
+
+class Solver:
+	class Cell:
+		var pos: Vector2
+		var previous: Cell
+		
+		func _init(cellPos:Vector2, previousCell: Cell) -> void:
+			pos = cellPos
+			previous = previousCell
+		
+		func get_position() -> Vector2:
+			return pos
+		
+		func get_previous() -> Cell:
+			return previous
+		
+	enum movementType {
+			CARDINAL,
+		DIAGONAL,
+		OMNIDIRECTIONAL
+	}
+	
+	#var maze: AStarMaze
+	#var unvisited: PackedVector2Array
+	#var visited: Array[Cell]
+	var directions: Array[Vector2]
+	var cachedSolution: PackedVector2Array
+	
+	func _init(movement: movementType) -> void:
+		switch_movement(movement)
+	
+	func switch_movement(movement: movementType) -> void:
+		match movement:
+			movementType.CARDINAL:
+				directions = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)] # Cardinal
+			movementType.DIAGONAL:
+				directions = [Vector2(1, 1), Vector2(1, -1), Vector2(-1, -1), Vector2(-1, 1)] # Diagonals
+			movementType.OMNIDIRECTIONAL:
+				directions = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1), Vector2(1, 1), Vector2(1, -1), Vector2(-1, -1), Vector2(-1, 1)] # Cardinal + Diagonal
+		
+	func solve_maze(maze: AStar.Maze) -> PackedVector2Array:
+		print("Starting Solve")
+		var start_time: int = Time.get_ticks_msec()
+		clear_cache()
+		var start: Vector2 = maze.get_start()
+		var goal: Vector2 = maze.get_end()
+		if (start == goal):
+			print("AStar [ERROR]: Invalid start/end")
+			var totalTime: int = Time.get_ticks_msec() - start_time
+			print("Time taken: " + str(totalTime) + " ms")
+			return []
+			
+		var unvisited: Array[Cell] = [Cell.new(start, null)]
+		var visited: Array[Cell] = []
+		
+		while (!unvisited.is_empty()):
+			if (unvisited[0].get_position() == goal):
+				cachedSolution = [unvisited[0].get_position()]
+				var nextCell = unvisited[0].get_previous()
+				
+				while (nextCell != null):
+					cachedSolution.append(nextCell.get_position())
+					nextCell = nextCell.get_previous()
+				
+				var totalTime: int = Time.get_ticks_msec() - start_time
+				print("Time taken: " + str(totalTime) + " ms")
+				return cachedSolution
+			var neighbors: Array[Cell] = get_neighbors(unvisited.pop_front(), maze, visited)
+			unvisited.append_array(neighbors)
+			visited.append_array(neighbors)
+		
+		print("No path found")
+		return []
+	
+	func get_neighbors(cell: Cell, maze: AStar.Maze, visited: Array[Cell]) -> Array[Cell]:
+		var neighbors: Array[Cell] = []
+		var mazeSize: Vector2 = maze.get_size()
+		
+		for dir in directions:
+			var newPos: Vector2 = cell.get_position() + dir
+			if (newPos.x >= 0 && newPos.y >= 0 && newPos.x < mazeSize.x && newPos.y < mazeSize.y): # Make sure newPos exists in maze
+				if (maze.is_tile_empty(newPos.x, newPos.y)): # Check if tile is empty
+					var alreadyVisited: bool = false
+					for visitedCell in visited:
+						if (newPos == visitedCell.get_position()):
+							alreadyVisited = true
+							break
+					if (!alreadyVisited): # Check if unvisited
+						neighbors.append(Cell.new(newPos, cell))
+		
+		return neighbors
+	
+	func has_cached_solution() -> bool:
+		return !cachedSolution.is_empty()
+	
+	func get_cached_solution() -> PackedVector2Array:
+		return cachedSolution
+	
+	func clear_cache() -> void:
+		cachedSolution = []
+
+
+
+class Maze:
+	var gridSize: Vector2
+	var grid: PackedInt32Array
+	
+	func _init(size = Vector2(10, 10)) -> void:
+		gridSize = size
+		grid = []
+		grid.resize(gridSize.x * gridSize.y)
+		grid.set(0, 2)
+		grid.set(grid.size()-1, 3)
+	
+	func change_tile(x: int, y: int, newTile: int) -> void:
+		var index: int = y*gridSize.x + x
+		grid[index] = newTile # Flip tile state
+	
+	func is_tile_empty(x: int, y: int) -> bool:
+		return grid[y*gridSize.x + x] != 1
+	
+	func get_tile_type(x: int, y: int) -> int:
+		return grid[y*gridSize.x + x]
+	
+	func get_start() -> Vector2:
+		for tile in len(grid):
+			if (grid[tile] == 2):
+				return Vector2(tile%int(gridSize.x), floor(tile/gridSize.x))
+		return Vector2.ZERO
+	
+	func get_end() -> Vector2:
+		for tile in len(grid):
+			if (grid[tile] == 3):
+				return Vector2(tile%int(gridSize.x), floor(tile/gridSize.x))
+		return Vector2.ZERO
+	
+	func get_size() -> Vector2:
+		return gridSize
---
+++ AStarHandler/astar_tester.gd
@@ -0,0 +1,102 @@
+extends Node2D
+
+@export_category("Maze")
+@export var gridSize: Vector2 = Vector2(10, 10)
+@export var tileSize: int = 16
+
+@export_category("Colors")
+@export var emptyColor: Color = Color(1.0, 1.0, 1.0) # 0
+@export var fillColor: Color = Color(0.0, 0.0, 0.0) # 1
+@export var startColor: Color = Color(0.0, 1.0, 0.0) # 2
+@export var goalColor: Color = Color(1.0, 1.0, 0.0) # 3
+@export var solutionColor: Color = Color(1.0, 0.0, 0.0) # 4
+@export var outlineColor: Color = Color(0.5, 0.5, 0.5)
+
+@export_category("Misc")
+@export var outlineSize: int = 1
+
+var maze: AStar.Maze
+var solver: AStar.Solver
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	maze = AStar.Maze.new(gridSize)
+	solver = AStar.Solver.new(AStar.Solver.movementType.CARDINAL)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	handle_click()
+
+func _draw() -> void:
+	for row in gridSize.y:
+		for col in gridSize.x:
+			draw_square(col*tileSize, row*tileSize, maze.grid[row*gridSize.x + col]) # Draw all squares
+	if (solver.has_cached_solution()):
+		draw_solution(solver.get_cached_solution())
+		solver.clear_cache()
+
+func draw_square(x: int, y: int, tileType: int) -> void:
+	var rect = Rect2(x, y, tileSize, tileSize)
+	match tileType:
+		0:
+			draw_rect(
+				rect,
+				emptyColor
+			)
+		1:
+			draw_rect(
+				rect,
+				fillColor
+			)
+		2:
+			draw_rect(
+				rect,
+				startColor
+			)
+		3:
+			draw_rect(
+				rect,
+				goalColor
+			)
+		4:
+			draw_rect(
+				rect,
+				solutionColor
+			)
+	
+	draw_rect( # Draw tile outline
+		rect,
+		outlineColor,
+		false,
+		outlineSize
+	)
+
+func handle_click() -> void:
+	if Input.is_action_pressed("left_click"):
+		var gridPos: Vector2 = floor(get_global_mouse_position()/tileSize)
+		if (gridPos.x >= 0 && gridPos.y >= 0 && gridPos.x < gridSize.x && gridPos.y < gridSize.y):
+			if (maze.get_tile_type(gridPos.x, gridPos.y) == 0):
+				maze.change_tile(
+					gridPos.x,
+					gridPos.y,
+					1
+				)
+				queue_redraw()
+	elif Input.is_action_pressed("right_click"):
+		var gridPos: Vector2 = floor(get_global_mouse_position()/tileSize)
+		if (gridPos.x >= 0 && gridPos.y >= 0 && gridPos.x < gridSize.x && gridPos.y < gridSize.y):
+			if (maze.get_tile_type(gridPos.x, gridPos.y) == 1):
+				maze.change_tile(
+					gridPos.x,
+					gridPos.y,
+					0
+				)
+				queue_redraw()
+
+func draw_solution(solution: PackedVector2Array) -> void:
+	for pos: Vector2 in solution:
+		draw_square(pos.x*tileSize, pos.y*tileSize, 4)
+
+func solve_maze() -> void:
+	solver.solve_maze(maze)
+	queue_redraw()
